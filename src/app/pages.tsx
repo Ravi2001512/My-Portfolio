99b583b12db1c9d070ebd747b82f4e9a2dd01df0
Ravindu Deshan <116144019+Ravi2001512@users.noreply.github.com>
2026-07-16 12:09:45 +0530

    make file structure

--- src/app/pages.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello Next.js 🚀
-      </h1>
-    </main>
-  );
-}
