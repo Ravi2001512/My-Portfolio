6613849004df3ccb4abf8f57bad4ab1fa4d2bed5
Ravindu Deshan <116144019+Ravi2001512@users.noreply.github.com>
2026-07-16 11:58:26 +0530

    initial commit

---
+++ src/app/pages.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Next.js 🚀
+      </h1>
+    </main>
+  );
+}
